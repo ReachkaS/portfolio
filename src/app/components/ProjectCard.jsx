5d130abe10f5bd4d53ba2dda4f636fcca3811c1d
sreachka <sreachka@gail.com>
2024-10-24 22:46:19 +0700

    change hover

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -12,7 +12,7 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => (
     >
         <div className="overlay absolute rounded-xl top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 group-hover:bg-opacity-80 transition-all duration-500"></div>
 
-        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+        <div className="absolute top-0 left-0 w-full h-full flex items-start lg:items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
             <Link href={gitUrl} className='relative rounded-full' aria-label="View code">
                 <CodeBracketIcon className='h-10 w-10 text-white m-2 cursor-pointer' />
             </Link>
